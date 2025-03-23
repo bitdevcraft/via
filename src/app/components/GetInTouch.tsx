a2f0daf7f3589671618a014612d46eb59278b80f
The Bit Dev <ryanvincent.capio@outlook.com>
2025-03-23 23:51:16 +0400

    remove unused imports

--- src/app/components/GetInTouch.tsx
+++ src/app/components/GetInTouch.tsx
@@ -1,6 +1,5 @@
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import Link from "next/link";
 
 export default function GetInTouch() {
   return (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
 import type { Metadata } from "next";
-import {
-  Inter,
-  Unna,
-  Cormorant_Infant,
-  Montserrat,
-  Anton_SC,
-} from "next/font/google";
+import { Inter, Montserrat, Anton_SC } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
